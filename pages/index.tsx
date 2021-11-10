2897ee8a57256b0755d9bed19aa14826611fce9f
Ivan C <chebiva@gmail.com>
2021-11-10 15:50:48 -0800

    ts ignore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import { useUser } from '@auth0/nextjs-auth0';
+import Link from 'next/link';
 import { AwesomeLink } from '../components/AwesomeLink';
-import { gql, useQuery } from '@apollo/client';
 
 const AllLinksQuery = gql`
   query allLinksQuery($first: Int, $after: String) {
@@ -12,6 +14,7 @@ const AllLinksQuery = gql`
       edges {
         cursor
         node {
+          index
           imageUrl
           url
           title
@@ -24,16 +27,29 @@ const AllLinksQuery = gql`
   }
 `;
 
-export default function Home() {
-  const { data, error, loading, fetchMore } = useQuery(AllLinksQuery, {
+function Home() {
+  const { user } = useUser();
+
+  const { data, loading, error, fetchMore } = useQuery(AllLinksQuery, {
     variables: { first: 2 },
   });
 
-  if (loading) return <p>Loading......</p>;
+  if (!user) {
+    return (
+      <div className="flex items-center justify-center">
+        To view the awesome links you need to{' '}
+        <Link href="/api/auth/login">
+          <a className=" block bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Login
+          </a>
+        </Link>
+      </div>
+    );
+  }
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
 
-  if (error) return <p>Oops, something went wrong {error.message}</p>;
-
-  const { hasNextPage, endCursor } = data.links.pageInfo;
+  const { endCursor, hasNextPage } = data.links.pageInfo;
 
   return (
     <div>
@@ -41,17 +57,21 @@ export default function Home() {
         <title>Awesome Links</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="container mx-auto max-w-5xl my-20">
+      <div className="container mx-auto max-w-5xl my-20 px-5">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {data?.links.edges.map(({ node }) => (
-            <AwesomeLink
-              title={node.title}
-              category={node.category}
-              url={node.url}
-              id={node.id}
-              description={node.description}
-              imageUrl={node.imageUrl}
-            />
+          {data?.links.edges.map(({ node }, i) => (
+            <Link href={`/link/${node.id}`} key={i}>
+              <a>
+                <AwesomeLink
+                  title={node.title}
+                  category={node.category}
+                  url={node.url}
+                  id={node.id}
+                  description={node.description}
+                  imageUrl={node.imageUrl}
+                />
+              </a>
+            </Link>
           ))}
         </div>
         {hasNextPage ? (
@@ -60,9 +80,13 @@ export default function Home() {
             onClick={() => {
               fetchMore({
                 variables: { after: endCursor },
+                //@ts-ignore
                 updateQuery: (prevResult, { fetchMoreResult }) => {
+                  //@ts-ignore
                   fetchMoreResult.links.edges = [
+                    //@ts-ignore
                     ...prevResult.links.edges,
+                    //@ts-ignore
                     ...fetchMoreResult.links.edges,
                   ];
                   return fetchMoreResult;
@@ -81,3 +105,5 @@ export default function Home() {
     </div>
   );
 }
+
+export default Home;
